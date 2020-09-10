b7c74f79936b6913c0b1640a01b4a6972dc7f649
Juliano Canuto <julianocanuto@gmail.com>
2020-09-10 20:54:23 -0300

    adicionado cabeçalho do plugin

---
+++ src/my-youtube-recommendation.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link              https://github.com/julianocanuto/desenvolvendo-plugin-wordpress
+ * @since             1.0.0
+ * @package           My_Youtube_Recommendation
+ *
+ * @wordpress-plugin
+ * Plugin Name:       My Youtube Recommendation
+ * Plugin URI:        https://github.com/julianocanuto/desenvolvendo-plugin-wordpress
+ * Description:       Display the last videos from a Youtube channel using Youtube feed and keep always updated even for cached posts.
+ * Version:           1.0.0
+ * Author:            Juliano Canuto
+ * Author URI:        https://github.com/julianocanuto/
+ * License:           GPLv3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       my-youtube-recommendation
+ * Domain Path:       /languages/
+ */
