b5a7c1768c58f903d8c3757d15cf0bf4ce3d1e86
Juliano Canuto <julianocanuto@gmail.com>
2020-09-11 17:57:45 -0300

    adicionado constantes e classes

--- src/my-youtube-recommendation.php
+++ src/my-youtube-recommendation.php
@@ -16,3 +16,49 @@
  * Text Domain:       my-youtube-recommendation
  * Domain Path:       /languages/
  */
+
+if (! defined('WPINC')) {
+     wp_die();
+}
+
+// Plugin Version
+if( ! defined('MY_YOUTUBE_RECOMMENDATION_VERSION')){
+   define('MY_YOUTUBE_RECOMMENDATION_VERSION', '1.0.0');
+}
+
+// Plugin Name
+if( ! defined('MY_YOUTUBE_RECOMMENDATION_NAME')){
+    define('MY_YOUTUBE_RECOMMENDATION_NAME', 'My Youtube Recommendation');
+ }
+
+// Plugin Slug
+if( ! defined('MY_YOUTUBE_RECOMMENDATION_SLUG')){
+    define('MY_YOUTUBE_RECOMMENDATION_SLUG', 'my-youtube-recommendation');
+}
+
+// Plugin Basename
+if( ! defined('MY_YOUTUBE_RECOMMENDATION_BASENAME')){
+    define('MY_YOUTUBE_RECOMMENDATION_BASENAME', plugin_basename(__FILE__));
+}
+
+// Plugin folder
+if( ! defined('MY_YOUTUBE_RECOMMENDATION_PLUGIN_DIR')){
+    define('MY_YOUTUBE_RECOMMENDATION_PLUGIN_DIR', plugin_dir_path(__FILE__));
+}
+
+// JSON File Name
+if( ! defined('MY_YOUTUBE_RECOMMENDATION_JSON_FILENAME')){
+    define('MY_YOUTUBE_RECOMMENDATION_JSON_FILENAME', 'my-yt-rec.json');
+}
+
+
+require_once MY_YOUTUBE_RECOMMENDATION_PLUGIN_DIR . 'includes/class-my-youtube-recommendation.php';
+require_once MY_YOUTUBE_RECOMMENDATION_PLUGIN_DIR . 'includes/class-my-youtube-recommendation-json.php';
+require_once MY_YOUTUBE_RECOMMENDATION_PLUGIN_DIR . 'includes/class-my-youtube-recommendation-shortcode.php';
+require_once MY_YOUTUBE_RECOMMENDATION_PLUGIN_DIR . 'includes/class-my-youtube-recommendation-widget.php';
+
+if (is_admin()){
+    require_once MY_YOUTUBE_RECOMMENDATION_PLUGIN_DIR . 'includes/class-my-youtube-recommendation-admin.php';
+}
+ 
+ 
