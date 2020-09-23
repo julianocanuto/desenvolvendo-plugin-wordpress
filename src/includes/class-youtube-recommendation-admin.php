3c7958881fb204dea692463228d65d7c74e1e3cf
Juliano Canuto <julianocanuto@gmail.com>
2020-09-23 17:08:44 -0300

    adicionado  método create_admin_page

--- src/includes/class-youtube-recommendation-admin.php
+++ src/includes/class-youtube-recommendation-admin.php
@@ -28,5 +28,34 @@ if (!class_exists('My_Youtube_Recommendation')) {
             add_filter( 'plugin_action_links_' . $this->plugin_basename, array($this, 'add_settings_link') );
         }
 
+        
+
+        public function add_plugin_page(){
+
+            add_options_page( 
+                __('Settings', 'my-youtube-recommendation'), 
+                __('My Youtube Recommendation', 'my-youtube-recommendation'), 
+                'manage_options', 
+                $this->plugin_slug, 
+                array($this, 'create_admin_page') 
+            );
+        }
+
+        
+        public function create_admin_page(){
+            ?>
+            <div class='wrap'>
+                <h1><?php echo __('My Youtube Recommendation' , 'my-youtube-recommendation'); ?></h1>
+                <form method="post" action="options.php">
+                    <?php
+                        settings_fields( 'my_yt_rec_options' );
+                        do_settings_sections( 'my-yt-rec-admin' );
+                        submit_button();
+                    ?>
+                </form>
+            </div>
+            <?php
+        }
+
     }
 }
